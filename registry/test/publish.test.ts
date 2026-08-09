d52ca8ab4eec42d026d094716ecdb84bc4817777
Roman Sirokov <roman@lunaris.app>
2026-08-09 20:03:25 +0200

    chore: target plugin SDK 0.0.2

--- registry/test/publish.test.ts
+++ registry/test/publish.test.ts
@@ -16,7 +16,7 @@ const manifest = {
   description: "A calendar plugin",
   developer: "Example",
   version: "1.0.0",
-  sdk: "^0.1.0",
+  sdk: "^0.0.2",
   modifications: [{ id: "example.calendar", type: "view" as const }],
 };
 
